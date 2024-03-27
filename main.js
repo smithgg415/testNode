f78d7eb03f4d8055cd1df4bab47dd799c131ff59
smithgg415 <133823261+smithgg415@users.noreply.github.com>
2024-03-27 20:33:22 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+let text = "Exibindo dados do usuário:"
+const dados = ["User", "Telefone", "Endereço"];
+let user = ["Smith", "(11) 22233 - 4455", "Rua Dolores"];
+
+console.log(text, dados, user);
